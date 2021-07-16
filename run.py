01bbc73460e9cc9b46671f930d8a3275f49d76ce
madgagarin <madgagarin@gmail.com>
2021-07-17 02:39:50 +0300

    error

--- run.py
+++ run.py
@@ -10,13 +10,6 @@ from tools import shutdown, startup
 from uwsgidecorators import thread
 
 
-@thread
-def sa():
-    loop = new_event_loop()
-    asyncio.set_event_loop(loop)
-    # loop.run_in_executor(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
-    loop.run_until_complete(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
-
 
 if __name__ == "__main__":
     # Thread(target=new_event_loop().run_until_complete, args=(abot.run(),),
