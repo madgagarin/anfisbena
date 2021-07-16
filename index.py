b38e3265917c3c000f71fe763c9e0032775be7b0
madgagarin <madgagarin@gmail.com>
2021-07-17 02:07:24 +0300

    uwsgi workd

--- index.py
+++ index.py
@@ -1,17 +1,39 @@
+import asyncio
+from asyncio import new_event_loop
+from threading import Thread
+
+from aiogram.utils import executor
 from flask_caching import Cache
 from flask import Flask, render_template, send_from_directory, abort, send_file, url_for, render_template_string
 
 # from images import gen_thumbnails
 # from misc import cache, bot
-from misc import db, bot as bott
+from uwsgidecorators import thread
+
+from misc import db, bot as bott, dp
 from flask_bootstrap import Bootstrap
+
+from tools import startup, shutdown
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-
 # cache.init_app(app)  # инициализация кэша
 # @app.before_first_request декоратор для функции запускаемой при старте
+
+import handlers
+import keyboards
+
+
+@thread
+def sa():
+    loop = new_event_loop()
+    asyncio.set_event_loop(loop)
+    # loop.run_in_executor(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
+    loop.run_until_complete(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
+
+
+sa()
 
 
 # хендлер корневой страницы
--- run.py
+++ run.py
@@ -1,14 +1,25 @@
-from index import app
-from aiogram import executor
-from misc import dp
-from tools import shutdown, startup
+import asyncio
+from asyncio import new_event_loop
 from threading import Thread
 
-# Импорт пакетов с хэндлерами и клавиатурами
-import handlers
-import keyboards
+from aiogram import executor
 
-# Запуск бота
+from index import app
+from misc import dp
+from tools import shutdown, startup
+from uwsgidecorators import thread
+
+
+@thread
+def sa():
+    loop = new_event_loop()
+    asyncio.set_event_loop(loop)
+    # loop.run_in_executor(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
+    loop.run_until_complete(executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True))
+
+
 if __name__ == "__main__":
-    Thread(target=app.run, args=(), daemon=True).start()  # Запуск дополнительного потока
-    executor.start_polling(dp, on_startup=startup, on_shutdown=shutdown, skip_updates=True)  # Запуск основного потока с ботом
+    # Thread(target=new_event_loop().run_until_complete, args=(abot.run(),),
+    #    daemon=True).start()  # Запуск дополнительного потока с новой асинхронной петлей внутри
+    #Thread(target=sa, args=(), daemon=True).start()  # Запуск дополнительного потока
+    app.run()
