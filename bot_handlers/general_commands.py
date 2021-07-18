33f78c0b23c1ef6af1a1f56ab65186ecbf3d8c00
madgagarin <madgagarin@gmail.com>
2021-07-19 01:16:13 +0300

    правим недочеты

--- bot_handlers/general_commands.py
+++ bot_handlers/general_commands.py
@@ -16,5 +16,5 @@ async def start_command(message: types.Message, state: FSMContext):
 # Реакция на команду /help
 @dp.message_handler(commands=['help'], state='*')
 async def help_command(message: types.Message):
-    await message.reply('Сообщение отправленное боту будет опубликовано https://anfisbena.herokuapp.com/', reply_markup=main_kb)
+    await message.reply('Сообщение, отправленное боту, будет опубликовано:\nhttps://anfisbena.herokuapp.com/', reply_markup=main_kb)
     await bot.send_message(admin_id, f'От {message.chat.username} отправлена команда /help')
